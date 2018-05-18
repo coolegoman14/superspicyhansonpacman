5e8566d8bb189ba2b77b364437b0be08f5a646e8
coolegoman14 <39099409+coolegoman14@users.noreply.github.com>
2018-05-18 11:13:15 -0700

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -1,105 +1,26 @@
-//import java.awt.event.*;
-//import javax.swing.*;
-//
-//import sun.audio.*;
-//
-//import java.io.*;
-//
-//public class Sound {
-////    public static String filename;
-////
-////    public Sound(String file) {
-////        filename = file;
-////    }
-//
-//    public static void main(String[] args) {
-//        JFrame frame = new JFrame();
-//        frame.setSize(300, 300);
-//        JButton button = new JButton("press to play");
-//        frame.add(button);
-//        button.addActionListener(new AL());
-//        frame.show(true);
-//    }
-//
-//    public static class AL implements ActionListener {
-//        public final void actionPerformed(ActionEvent e) {
-//            play();
-//        }
-//    }
-//
-//    public static void play() {
-//        //Plays Audio
-//        AudioPlayer MGP = AudioPlayer.player;
-//        //Gives file name of .wav files (  no mp3 :( "audionamehere.wav"  )
-//        AudioStream BGM;
-//        //Receives data from BGM in order to create new ContinuousAudioDataStream
-//        AudioData MD;
-//        //provides necessary data to audio player (MGP) to play the sound
-//        ContinuousAudioDataStream loop = null;
-//
-//        try {
-//            BGM = new AudioStream(new FileInputStream("litty.wav"));
-//            MD = BGM.getData();
-//            loop = new ContinuousAudioDataStream(MD);
-//        } catch (IOException error) {}
-//
-//        MGP.start(loop);
-//    }
-//}
-//
-////https://www.youtube.com/watch?v=VMSTTg5EEnY
-//
-///* Sound Guide
-//Music that plays in the beginning before the game starts: https://www.youtube.com/watch?v=HwyAwPLHqnM
-//Music that plays in the background when pacman does not have a power pellet: https://www.youtube.com/watch?v=lIx8TrlSV7I
-//Music that plays when pacman eats the normie pellets (little pellets): https://www.youtube.com/watch?v=5DE2AtHVzBU
-//Music that plays in the background when pacman has eaten a power pellet: https://www.youtube.com/watch?v=XgTaPJRa4PE
-//Sound that plays when pacman dies: https://www.youtube.com/watch?v=mD4nMp90MYQ
-//Sound that plays when pacman eats a ghost: https://www.youtube.com/watch?v=CwZPo2xx4iY
-//Sound that plays when pacman eats fruit: https://www.youtube.com/watch?v=33FKY1uIrG0
-// */
-
-import java.awt.event.*;
-import javax.swing.*;
-import sun.audio.*;
 import java.io.*;
+import javax.sound.sampled.*;
 
 public class Sound {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setSize(300, 300);
-        JButton button = new JButton("press to play");
-        frame.add(button);
-        button.addActionListener(new AL());
-        frame.show(true);
+        File yeet = new File("litty.wav");
+        PlaySound(yeet);
     }
-    public static class AL implements ActionListener {
-        public final void actionPerformed(ActionEvent e) {
-            play();
-        }
+    
+    public static void PlaySound(File Sound)
+    {
+    	try {
+    		Clip clip = AudioSystem.getClip();
+    		clip.open(AudioSystem.getAudioInputStream(Sound));
+    	    clip.start();
+    	    
+    	    Thread.sleep(clip.getMicrosecondLength()/10);
+    	}catch(Exception e) {
+    		
+    	}
     }
-    public static void play() {
-        AudioPlayer MGP = AudioPlayer.player;
-        AudioStream BGM;
-        AudioData MD;
-        ContinuousAudioDataStream loop = null;
-        try {
-            BGM = new AudioStream(new FileInputStream("litty.wav"));
-            MD = BGM.getData();
-            loop = new ContinuousAudioDataStream(MD);
-        } catch (IOException error) {}
-        MGP.start(loop);
-    }
+   
+    
 }
-
-//https://www.youtube.com/watch?v=VMSTTg5EEnY
-
-/* Sound Guide
-Music that plays in the beginning before the game starts: https://www.youtube.com/watch?v=HwyAwPLHqnM
-Music that plays in the background when pacman does not have a power pellet: https://www.youtube.com/watch?v=lIx8TrlSV7I
-Music that plays when pacman eats the normie pellets (little pellets): https://www.youtube.com/watch?v=5DE2AtHVzBU
-Music that plays in the background when pacman has eaten a power pellet: https://www.youtube.com/watch?v=XgTaPJRa4PE
-Sound that plays when pacman dies: https://www.youtube.com/watch?v=mD4nMp90MYQ
-Sound that plays when pacman eats a ghost: https://www.youtube.com/watch?v=CwZPo2xx4iY
-Sound that plays when pacman eats fruit: https://www.youtube.com/watch?v=33FKY1uIrG0
- */
+//https://www.youtube.com/watch?v=3q4f6I5zi2w
+//https://www.youtube.com/watch?v=QVrxiJyLTqU
