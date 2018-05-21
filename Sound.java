217010717a1d4acf904d38c87c626d3c1818ca11
coolegoman14 <39099409+coolegoman14@users.noreply.github.com>
2018-05-21 11:12:07 -0700

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -2,16 +2,30 @@ import java.io.*;
 import javax.sound.sampled.*;
 
 public class Sound {
-    public static void main(String[] args) {
-        File yeet = new File("litty.wav");
-        PlaySound(yeet);
+	private File yeet;
+	private Clip clip;
+//    public static void main(String[] args) {
+//        File yeet = new File("litty.wav");
+//        PlaySound(yeet);
+//    }
+    public Sound(String s) {
+    	yeet = new File(s);
+    	try {
+			clip = AudioSystem.getClip();
+			try {
+				AudioInputStream yayeet = AudioSystem.getAudioInputStream(yeet);
+				clip.open(yayeet);
+			} catch (UnsupportedAudioFileException e) {} 
+			catch (IOException e) {}
+		} catch (LineUnavailableException e) {
+			
+		}
     }
-    
-    public static void PlaySound(File Sound)
+    public void PlaySound()
     {
     	try {
     		Clip clip = AudioSystem.getClip();
-    		clip.open(AudioSystem.getAudioInputStream(Sound));
+    		clip.open(AudioSystem.getAudioInputStream(yeet));
     	    clip.start();
     	    
     	    Thread.sleep(clip.getMicrosecondLength()/10);
@@ -19,9 +33,11 @@ public class Sound {
     		
     	}
     }
+    public void loop() {
+    	clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
    
     
 }
 //https://www.youtube.com/watch?v=3q4f6I5zi2w
 //https://www.youtube.com/watch?v=QVrxiJyLTqU
-https://stackoverflow.com/questions/11919009/using-javax-sound-sampled-clip-to-play-loop-and-stop-mutiple-sounds-in-a-gamehttps://stackoverflow.com/questions/11919009/using-javax-sound-sampled-clip-to-play-loop-and-stop-mutiple-sounds-in-a-game
