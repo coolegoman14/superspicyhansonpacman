8d6fa6fead5a2fa852e4545c3c363b07798ffd79
coolegoman14 <39099409+coolegoman14@users.noreply.github.com>
2018-05-21 10:33:06 -0700

    Update Blinky.java

--- Blinky.java
+++ Blinky.java
@@ -2,20 +2,16 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 public class Blinky extends Ghost{
-		
-	
 	int x=36;
 	int y=36;
 	Rectangle clone;
-
+	
 	public Blinky(Pacman pacman) {
 		super(pacman);
 		clone= new Rectangle();
-		
 	}
 	
-	public void draw(Graphics2D g){
-		
+	public void draw(Graphics2D g){	
 		if(x!=pacman.x){
 			if(x<pacman.x && canMove(x+2,y)){
 				x+=3;
@@ -32,13 +28,12 @@ public class Blinky extends Ghost{
 				y-=3;
 			}
 		}
-	
 		clone.setFrameFromCenter(x+13, y+13, x, y);
 		g.draw(clone);
 		g.drawImage(Blinky, x, y, null);
 	}
-	public Boolean canMove(int x,int y){
-		
+	
+	public Boolean canMove(int x,int y){	
 			for(Rectangle r: Board.wallList){
 				if(clone.intersects(r.getX()-9, r.getY()-9, r.getWidth()-9, r.getHeight()-9)){
 					return false;
