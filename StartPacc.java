8301b7f41abca8fb407e8cd16c7cfa7cf1ce992d
coolegoman14 <39099409+coolegoman14@users.noreply.github.com>
2018-05-25 10:58:50 -0700

    Add files via upload
    StartPacc with sound as of 5/25/2018

--- StartPacc.java
+++ StartPacc.java
@@ -2,51 +2,63 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.Timer;
 
 public class StartPacc {
-	
+
 	private JFrame frame = new JFrame("PAC-MAN");
 	private JPanel panel;
 	Timer timer = new Timer(50,null);
-	int speed=6;
+	int time = 0;
 	Board board= new Board();
 	Pacman pacman= new Pacman();
 	Blinky blinky= new Blinky(pacman);
 	Pinky pinky= new Pinky(pacman);
-	
-
-	public StartPacc() {
-		
+	Inky inky= new Inky(pacman);
+	Clyde clyde= new Clyde(pacman);
+	Sound[] master = new Sound[10];{
+		master[0] = new Sound("intro.wav");
+		master[1] = new Sound("SirenSound.wav");
+		master[2] = new Sound("wakaflocka.wav");
+		master[3] = new Sound("death.wav");
 	}
 	public static void main(String[] args){
 		new StartPacc().startGame();
 	}
-	
+
 	private void startGame() {
-		
+
 		panel = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
 				board.drawBoard((Graphics2D) g);
-			
+
 				pacman.draw((Graphics2D) g);
-				blinky.draw((Graphics2D) g);
-				//pinky.draw((Graphics2D) g);
+				/*blinky.draw((Graphics2D) g);
+				pinky.draw((Graphics2D) g);
+				inky.draw((Graphics2D) g);
+				clyde.draw((Graphics2D) g);*/
 			}
 		};
 		setUpGame();
+		System.out.println(master[0].isRunning());
+		System.out.println(master[0].isActive());
+		master[0].getClip().start();
+		System.out.println("yeet");
+		//		while(master[0].isRunning()== true) {
+		//			System.out.println(master[0].isRunning());
+		//			System.out.println(master[0].isActive());
+		//		}
+		System.out.println(master[0].isActive());
 		setUpKeyMappings();
-		
-		
 		timer.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -54,159 +66,77 @@ public class StartPacc {
 			}
 		});
 		timer.start();
-	}
-	
-	protected void tick() {
-		
-		if(pacman.direction==Pacman.Direction.RIGHT){
-				if(canMove(Pacman.Direction.RIGHT)){
-				pacman.x+=speed;
-				}
 
-			if(pacman.startAngle==70){
-				pacman.isIncreasing=false;
-			}
-			else if(pacman.startAngle==0){
-				pacman.isIncreasing=true;
-			}
-		}
-		
-		if(pacman.direction==Pacman.Direction.LEFT){	
-			if(canMove(Pacman.Direction.LEFT)){
-				pacman.x-=speed;
-			}
-			
-			if(pacman.startAngle==250){
-				pacman.isIncreasing=false;
-			}
-			else if(pacman.startAngle==180){
-				pacman.isIncreasing=true;
-			}
-		}
-		
-		if(pacman.direction==Pacman.Direction.UP){
-			if(canMove(Pacman.Direction.UP)){
-				pacman.y-=speed;
-			}
-			
-			if(pacman.startAngle==160){
-				pacman.isIncreasing=false;
-			}
-			else if(pacman.startAngle==90){
-				pacman.isIncreasing=true;
-			}
-		}
-		
-		if(pacman.direction==Pacman.Direction.DOWN){
-			if(canMove(Pacman.Direction.DOWN)){
-				pacman.y+=speed;
-			}
-			
-			if(pacman.startAngle==340){
-				pacman.isIncreasing=false;
-			}
-			else if(pacman.startAngle==270){
-				pacman.isIncreasing=true;
-			}
-		}
-		
-		if(pacman.isIncreasing){
-			pacman.startAngle+=10;
-			pacman.arcAngle-=20;
-			}
-			else{
-				pacman.startAngle-=10;
-				pacman.arcAngle+=20;
-			}
-		panel.repaint();
 	}
-	
+
+	protected void tick() {
+		time += 50;
+		if(time>4000) {
+			if (pacman.direction!=pacman.userDirection && pacman.canMove(pacman.userDirection)) {
+				pacman.direction=pacman.userDirection;
+				pacman.changeAngleAttributes(pacman.direction);
+
+			}
+			pacman.move();
+			pacman.doMouthAnimation();
+			panel.repaint();
+		}
+	}
+
 	private void setUpKeyMappings() {
 
 		panel.getInputMap().put(KeyStroke.getKeyStroke("LEFT"),"left");
 		panel.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"),"right");
 		panel.getInputMap().put(KeyStroke.getKeyStroke("UP"),"up");
 		panel.getInputMap().put(KeyStroke.getKeyStroke("DOWN"),"down");
-		
+
 		panel.getActionMap().put("left",new AbstractAction(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				pacman.userDirection=Pacman.Direction.LEFT;
+				if(pacman.canMove(Pacman.Direction.LEFT)){
+					pacman.direction=Pacman.Direction.LEFT;
+					pacman.changeAngleAttributes(Pacman.Direction.LEFT);
 
-				if(canMove(Pacman.Direction.LEFT)){
-				pacman.direction=Pacman.Direction.LEFT;
-				pacman.arcAngle=360;
-				pacman.startAngle=180;
 				}
 			}
 		});
 		panel.getActionMap().put("right",new AbstractAction(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(canMove(Pacman.Direction.RIGHT)){
-				pacman.direction=Pacman.Direction.RIGHT;
-				pacman.arcAngle=360;
-				pacman.startAngle=0;
+				pacman.userDirection=Pacman.Direction.RIGHT;
+				if(pacman.canMove(Pacman.Direction.RIGHT)){
+					pacman.direction=Pacman.Direction.RIGHT;
+					pacman.changeAngleAttributes(Pacman.Direction.RIGHT);
+
 				}
-				
+
 			}
 		});
 		panel.getActionMap().put("up",new AbstractAction(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(canMove(Pacman.Direction.UP)){
-				pacman.direction=Pacman.Direction.UP;
-				pacman.arcAngle=360;
-				pacman.startAngle=90;
+				pacman.userDirection=Pacman.Direction.UP;
+				if(pacman.canMove(Pacman.Direction.UP)){
+					pacman.direction=Pacman.Direction.UP;
+					pacman.changeAngleAttributes(Pacman.Direction.UP);
+
 				}
-				
 			}
 		});
 		panel.getActionMap().put("down",new AbstractAction(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(canMove(Pacman.Direction.DOWN)){
-				pacman.direction=Pacman.Direction.DOWN;
-				pacman.arcAngle=360;
-				pacman.startAngle=270;
+				pacman.userDirection=Pacman.Direction.DOWN;
+				if(pacman.canMove(Pacman.Direction.DOWN)){
+					pacman.direction=Pacman.Direction.DOWN;
+					pacman.changeAngleAttributes(Pacman.Direction.DOWN);
 				}
 			}
 		});
 		panel.requestFocusInWindow();
 	}
-	
-	public Boolean canMove(Pacman.Direction direction){
-		if(direction==Pacman.Direction.RIGHT){
-			for(Rectangle r: Board.wallList){
-				if(pacman.clone.intersects(r.getX()-12, r.getY(), r.getWidth(), r.getHeight())){
-					return false;
-				}
-			}
-		}
-		if(direction==Pacman.Direction.LEFT){
-			for(Rectangle r: Board.wallList){
-				if(pacman.clone.intersects(r.getX()+9, r.getY(), r.getWidth(), r.getHeight())){
-					return false;
-				}
-			}
-		}
-		if(direction==Pacman.Direction.UP){
-			for(Rectangle r: Board.wallList){
-				if(pacman.clone.intersects(r.getX(), r.getY()+9, r.getWidth(), r.getHeight())){
-					return false;
-				}
-			}
-		}
-		if(direction==Pacman.Direction.DOWN){
-			for(Rectangle r: Board.wallList){
-				if(pacman.clone.intersects(r.getX(), r.getY()-9, r.getWidth(), r.getHeight())){
-					return false;
-				}
-			}
-		}
-		
-		return true;
-	}
-	
+
 	private void setUpGame(){
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
@@ -216,5 +146,5 @@ public class StartPacc {
 		panel.setBackground(Color.black);
 		this.setUpKeyMappings();
 	}
-
 }
+
