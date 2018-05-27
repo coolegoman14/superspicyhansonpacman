062b6a053dba4b07931a14f6eb4d7ad6a01d1d5f
Aw205 <39099545+Aw205@users.noreply.github.com>
2018-05-26 17:40:33 -0700

    Add files via upload

--- Blinky.java
+++ Blinky.java
@@ -1,44 +1,111 @@
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Blinky extends Ghost{
-	int x=36;
-	int y=36;
-	Rectangle clone;
+		
 	
+	int x=32;
+	int y=32;
+	int centerX=32;
+	int centerY=32;
+	
+	Rectangle clone;
+	Direction direction=Ghost.Direction.RIGHT;
+	Direction futureDirection;
+	List<Tile> openList= new ArrayList<Tile>();
+	List<Tile> closedList= new ArrayList<Tile>();
+	List<Tile> adjacentTiles= new ArrayList<Tile>();
+	static List<Tile> finalPath= new ArrayList<Tile>();
+	Tile startTile=Board.tileArray[y/Board.TILE_D][x/Board.TILE_D];
+	Tile finalTile;
+	Tile smallest;
+	Tile current;
+
 	public Blinky(Pacman pacman) {
 		super(pacman);
 		clone= new Rectangle();
 	}
 	
-	public void draw(Graphics2D g){	
-		if(x!=pacman.x){
-			if(x<pacman.x && canMove(x+2,y)){
-				x+=3;
-			}
-			else if(canMove(x-2,y)){
-				x-=3;
-			}
-		}
-		if(y!=pacman.y){
-			if(y<pacman.y && canMove(x,y+2)){
-				y+=3;
-			}
-			else if(canMove(x,y-2)){
-				y-=3;
-			}
-		}
-		clone.setFrameFromCenter(x+13, y+13, x, y);
-		g.draw(clone);
-		g.drawImage(Blinky, x, y, null);
+	public void draw(Graphics2D g){
+		
+		findPath();
+		//move();
+		
+		/*clone.setFrameFromCenter(x+16, y+16, x, y);
+		g.setColor(Color.DARK_GRAY);
+		g.draw(clone);*/
+		g.drawImage(Blinky, x-5, y-3, null);
+		openList.clear();
+		closedList.clear();
+		
 	}
 	
-	public Boolean canMove(int x,int y){	
-			for(Rectangle r: Board.wallList){
-				if(clone.intersects(r.getX()-9, r.getY()-9, r.getWidth()-9, r.getHeight()-9)){
-					return false;
+	public void move() {
+		moveTo(finalPath.get(1));
+	}
+	
+	public void moveTo(Tile tile){
+		while(centerX<tile.centerX){
+			x+=speed;
+		}
+		while(centerX>tile.centerX){
+			x-=speed;
+		}
+		while(centerY<tile.centerY){
+			y+=speed;
+		}
+		while(centerY>tile.centerY){
+			y-=speed;
+		}
+	}
+	
+	public void findPath(){
+		Tile targetTile=Board.tileArray[Pacman.centerY/Board.TILE_D][Pacman.centerX/Board.TILE_D];
+		openList.add(startTile);
+
+		while(!openList.isEmpty()){
+			current=findSmallestNode();
+			openList.remove(current);
+			closedList.add(current);
+				if(closedList.contains(targetTile)){
+					break;
+				}
+				findAdjacentTiles(current,adjacentTiles);
+				for(Tile tile: adjacentTiles){
+					if(closedList.contains(tile)){
+						continue;
+					}
+					if(!openList.contains(tile)){
+						tile.parent=current;
+						tile.calculateFHG();
+						openList.add(tile);
+					}
+					else{
+						
+					}	
+				}
+		}
+		finalTile=targetTile;
+		while(finalTile!=startTile){
+			finalTile= finalTile.parent;
+			finalPath.add(finalTile);
+		}
+	}
+	
+	
+	
+	public Tile findSmallestNode(){
+		 smallest=openList.get(0);
+		for(int i=0;i<openList.size();i++){
+			if(openList.get(i).Fscore<smallest.Fscore){
+				smallest=openList.get(i);
 			}
 		}
-		return true;
+		return smallest;
 	}
 }
+
--- Ghost.java
+++ Ghost.java
@@ -1,8 +1,8 @@
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -13,14 +13,21 @@ public abstract class Ghost {
 	Image Clyde;
 	Image Inky;
 	Pacman pacman;
+	int speed=6;
+	int centerX;
+	int centerY;
+	Direction direction;
 	
+	enum Direction{
+		RIGHT,LEFT,UP,DOWN;
+	}
 	public Ghost(Pacman pacman) {
 		this.pacman=pacman;
 		try {
 			
 			Blinky=ImageIO.read(new File("Blinky.png")).getScaledInstance(50, 50,Image.SCALE_SMOOTH);
-			Pinky=ImageIO.read(new File("Pinky.png")).getScaledInstance(50, 50,Image.SCALE_SMOOTH);
-			Clyde=ImageIO.read(new File("Clyde.png")).getScaledInstance(50, 50,Image.SCALE_SMOOTH);
+			Pinky=ImageIO.read(new File("Pinky.png")).getScaledInstance(40, 40,Image.SCALE_SMOOTH);
+			Clyde=ImageIO.read(new File("Clyde.png")).getScaledInstance(40, 40,Image.SCALE_SMOOTH);
 			Inky=ImageIO.read(new File("Inky.png")).getScaledInstance(50, 50,Image.SCALE_SMOOTH);
 			
 		} catch (IOException e) {
@@ -28,10 +35,68 @@ public abstract class Ghost {
 		}
 		
 	}
-	
-	public void draw(Graphics2D g){
+public Boolean canMove(Ghost.Direction desiredDirection,Tile tile){
 		
+		if(desiredDirection==Ghost.Direction.RIGHT ){
+			if(direction!=Ghost.Direction.RIGHT){
+				if(Board.tileArray[tile.centerY/Board.TILE_D][(tile.centerX/Board.TILE_D)+1].isWall){
+					return false;
+				}
+			}
+			else if(Board.tileArray[tile.centerY/Board.TILE_D][tile.centerX/Board.TILE_D].centerX==centerX && Board.tileArray[tile.centerY/Board.TILE_D][(centerX/Board.TILE_D)+1].isWall){
+				return false;
+			}
+		}
+		if(desiredDirection==Ghost.Direction.LEFT ){
+			if(direction!=Ghost.Direction.LEFT){
+				if(Board.tileArray[tile.centerY/Board.TILE_D][(tile.centerX/Board.TILE_D)-1].isWall){
+					return false;
+				}
+			}
+			else if(Board.tileArray[tile.centerY/Board.TILE_D][tile.centerX/Board.TILE_D].centerX==centerX && Board.tileArray[tile.centerY/Board.TILE_D][(tile.centerX/Board.TILE_D)-1].isWall){
+				return false;
+			}
+		}
+		if(desiredDirection==Ghost.Direction.UP ){
+			if(direction!=Ghost.Direction.UP){
+				if(Board.tileArray[(tile.centerY/Board.TILE_D)-1][tile.centerX/Board.TILE_D].isWall){
+					return false;
+				}
+			}
+			else if(Board.tileArray[tile.centerY/Board.TILE_D][tile.centerX/Board.TILE_D].centerY==centerY && Board.tileArray[(tile.centerY/Board.TILE_D)-1][tile.centerX/Board.TILE_D].isWall){
+				return false;
+			}
+		}
+		if(desiredDirection==Ghost.Direction.DOWN ){
+			if(direction!=Ghost.Direction.DOWN){
+				if(Board.tileArray[(tile.centerY/Board.TILE_D)+1][tile.centerX/Board.TILE_D].isWall){
+					return false;
+				}
+			}
+			else if(Board.tileArray[tile.centerY/Board.TILE_D][tile.centerX/Board.TILE_D].centerY==centerY && Board.tileArray[(tile.centerY/Board.TILE_D)+1][tile.centerX/Board.TILE_D].isWall){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+
+public void findAdjacentTiles(Tile current, List<Tile> adjacentTiles){
+	adjacentTiles.clear();
+	if(canMove(Ghost.Direction.RIGHT,current)){
+		adjacentTiles.add(Board.tileArray[current.y/Board.TILE_D][(current.x/Board.TILE_D)+1]);
+	}
+	if(canMove(Ghost.Direction.LEFT,current)){
+		adjacentTiles.add(Board.tileArray[current.y/Board.TILE_D][(current.x/Board.TILE_D)-1]);
+	}
+	if(canMove(Ghost.Direction.UP,current)){
+		adjacentTiles.add(Board.tileArray[(current.y/Board.TILE_D)-1][current.x/Board.TILE_D]);
+	}
+	if(canMove(Ghost.Direction.DOWN,current)){
+		adjacentTiles.add(Board.tileArray[(current.y/Board.TILE_D)+1][current.x/Board.TILE_D]);
 	}
 	
-	
+}
+
 }
